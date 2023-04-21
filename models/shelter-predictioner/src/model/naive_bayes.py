22adff27cb5e2f54a2c24908b34f7284dbcd773e
volodymyrrybak <volodymyr.y.rybak@gmail.com>
2023-04-21 14:57:18 +0300

    Update bayes following TODO list

--- models/shelter-predictioner/src/model/naive_bayes.py
+++ models/shelter-predictioner/src/model/naive_bayes.py
@@ -1,12 +1,17 @@
+import pickle
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import GridSearchCV
+from sklearn.inspection import permutation_importance
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as ImbPipeline
+from utils import get_region_number
+
 
 def train():
     # 1. Load the data
@@ -14,7 +19,8 @@ def train():
 
     # 2. Preprocess the data
     # Create a binary label for whether an alarm occurred
-    data['vector'] = data['vector'].apply(lambda x: np.array([float(i) for i in x.strip('[]').split(',')], dtype=np.float32) if isinstance(x, str) else x)
+    data['vector'] = data['vector'].apply(
+        lambda x: np.array([float(i) for i in x.strip('[]').split(',')], dtype=np.float32) if isinstance(x, str) else x)
     data['vector_mean'] = data['vector'].apply(lambda x: np.mean(x) if isinstance(x, np.ndarray) else np.nan)
 
     # Impute missing values in 'vector_mean'
@@ -24,8 +30,7 @@ def train():
     data['alarm_occurred'] = np.where((data['alarm_start'].notna()) & (data['alarm_end'].notna()), 1, 0)
 
     # Encode the city names
-    le = LabelEncoder()
-    data['city_encoded'] = le.fit_transform(data['city'])
+    data['city_encoded'] = data['city'].apply(lambda x: get_region_number(str(x)))
 
     # Extract features from the date
     data['day_of_week'] = data['date'].dt.dayofweek
@@ -33,48 +38,68 @@ def train():
     data['month'] = data['date'].dt.month
 
     # Extract features and target
-    features = ['city_encoded', 'day_of_week', 'day_of_year', 'month', "day_feelslikemin", "day_sunriseEpoch",
-                "day_sunsetEpoch", "city_latitude", "city_longitude", "city_tzoffset", "day_feelslike",
-                "day_precipprob", "day_snow", "day_snowdepth", "day_windgust", "day_winddir", "day_pressure",
-                "day_cloudcover", "day_severerisk", 'is_rus_holiday', 'is_ukr_holiday', 'vector_mean']
+    features = ['city_encoded', 'day_of_week', 'day_of_year', "city_latitude", "city_longitude", "city_tzoffset",
+                "day_feelslike", "day_snow", "day_snowdepth", "day_windgust", "day_winddir", "day_pressure",
+                "day_precipprob", "day_severerisk", 'is_rus_holiday', 'is_ukr_holiday', 'vector_mean']
     X = data[features]
     y = data['alarm_occurred']
 
     # Split data into training and testing sets using TimeSeriesSplit
     tscv = TimeSeriesSplit(n_splits=5)
-    for train_index, test_index in tscv.split(X):
+
+    for j, (train_index, test_index) in enumerate(tscv.split(X)):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
         # Train the Naive Bayes model
-        pipe = Pipeline([
+        pipe = ImbPipeline([
+            ('sampling', SMOTE()),
             ('scaler', StandardScaler()),
             ('classifier', GaussianNB())
         ])
 
         # Define hyperparameters to search over
         param_grid = {
+            'sampling__k_neighbors': [1, 3, 5, 7],
             'classifier__var_smoothing': [1e-9, 1e-8, 1e-7, 1e-6]
         }
 
-        # Perform grid search using cross-validation
-        clf = GridSearchCV(pipe, param_grid, cv=5, scoring='accuracy')
+        # Perform grid search using cross-validation RandomizedSearchCV?
+        clf = GridSearchCV(pipe, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
         clf.fit(X_train, y_train)
+
+        result = permutation_importance(clf.best_estimator_, X_train, y_train, n_repeats=10, random_state=42)
+
+        # Print feature importances
+        for i, (importance, feature) in enumerate(zip(result.importances_mean, features)):
+            print(f"Feature {i}: {feature} - Importance: {importance}")
 
         # Print the best hyperparameters and their corresponding accuracy
         print("Best hyperparameters:", clf.best_params_)
-        print("Best accuracy:", clf.best_score_)
+        print("accuracy:", clf.best_score_)
 
-        # Check accuracy on the test set using the
+        # Check accuracy on the test set using the best model
+        y_pred = clf.predict(X_test)
+        print("Test set accuracy:", accuracy_score(y_test, y_pred))
+        print("Test classification_report:\n", classification_report(y_pred, y_test))
+        c_matrix = confusion_matrix(y_test, y_pred)
+        print("Test confusion_matrix:\n", c_matrix)
+
+        tn, fp, fn, tp = c_matrix.ravel()
+
+        print("Confusion matrix:")
+        print("True Negatives:", tn)
+        print("False Positives:", fp)
+        print("False Negatives:", fn)
+        print("True Positives:", tp)
+
+        print(j)
+
+        # save the model as a pickle file
+        if j == 4:
+            filename = 'naive_bayes.pkl'
+            with open(filename, 'wb') as f:
+                pickle.dump(clf, f)
 
 
-    # Check accuracy on the test set using the best model
-    y_pred = clf.predict(X_test)
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    print("Confusion matrix:")
-    print("True Negatives:", tn)
-    print("False Positives:", fp)
-    print("False Negatives:", fn)
-    print("True Positives:", tp)
+train()
--- models/shelter-predictioner/src/model/utils.py
+++ models/shelter-predictioner/src/model/utils.py
@@ -30,3 +30,58 @@ def tfIdfVector():
 
     with open('resources/dataset/vector.json', 'w') as outfile:
         json.dump(vectors, outfile, indent=2)
+
+
+def get_region_number(city):
+    if city == "Simferopol":
+        return 1
+    elif city == "Vinnytsia":
+        return 2
+    elif city == "Lutsk":
+        return 3
+    elif city == "Dnipro":
+        return 4
+    elif city == "Donetsk":
+        return 5
+    elif city == "Zhytomyr":
+        return 6
+    elif city == "Uzhgorod":
+        return 7
+    elif city == "Zaporozhye":
+        return 8
+    elif city == "Ivano-Frankivsk":
+        return 9
+    elif city == "Kyiv":
+        return 10
+    elif city == "Kropyvnytskyi":
+        return 11
+    elif city == "Luhansk":
+        return 12
+    elif city == "Lviv":
+        return 13
+    elif city == "Mykolaiv":
+        return 14
+    elif city == "Odesa":
+        return 15
+    elif city == "Poltava":
+        return 16
+    elif city == "Rivne":
+        return 17
+    elif city == "Sumy":
+        return 18
+    elif city == "Ternopil":
+        return 19
+    elif city == "Kharkiv":
+        return 20
+    elif city == "Kherson":
+        return 21
+    elif city == "Khmelnytskyi":
+        return 22
+    elif city == "Cherkasy":
+        return 23
+    elif city == "Chernivtsi":
+        return 24
+    elif city == "Chernihiv":
+        return 25
+    print("HERE " + city)
+    return -1
